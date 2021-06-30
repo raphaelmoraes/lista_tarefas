9245ddeb9da3326c458b50e96bf027ce9617b884
Raphael <viladapenha@gmail.com>
2021-06-30 20:06:15 -0300

    Adicionado widget Dismissible

--- lib/Home.dart
+++ lib/Home.dart
@@ -63,6 +63,41 @@ class _HomeState extends State<Home> {
     });
   }
 
+  Widget criarListaItem(context, index) {
+    final item = _listaTarefas[index]["titulo"];
+    return Dismissible(
+      key: Key(item),
+      direction: DismissDirection.endToStart,
+      onDismissed: (direction) {
+        _listaTarefas.removeAt(index);
+        _salvarArquivo();
+      },
+      background: Container(
+        color: Colors.red,
+        padding: EdgeInsets.all(16),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Icon(
+              Icons.delete,
+              color: Colors.white,
+            ),
+          ],
+        ),
+      ),
+      child: CheckboxListTile(
+        title: Text(_listaTarefas[index]['titulo']),
+        value: _listaTarefas[index]['realizada'],
+        onChanged: (valorAlterado) {
+          setState(() {
+            _listaTarefas[index]['realizada'] = valorAlterado;
+            _salvarArquivo();
+          });
+        },
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     _salvarArquivo();
@@ -113,21 +148,7 @@ class _HomeState extends State<Home> {
           Expanded(
             child: ListView.builder(
               itemCount: _listaTarefas.length,
-              itemBuilder: (context, index) {
-                /*   return ListTile(
-                  title: Text(_listaTarefas[index]['titulo']),
-                );
-               */
-                return CheckboxListTile(
-                    title: Text(_listaTarefas[index]['titulo']),
-                    value: _listaTarefas[index]['realizada'],
-                    onChanged: (valorAlterado) {
-                      setState(() {
-                        _listaTarefas[index]['realizada'] = valorAlterado;
-                        _salvarArquivo();
-                      });
-                    });
-              },
+              itemBuilder: criarListaItem,
             ),
           ),
         ],
